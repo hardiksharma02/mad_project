d663a49dc45c9b31256cac2a0b4a4f02704ab607
hardiksharma02 <211b126@juetguna.in>
2023-05-11 17:02:01 +0530

    files sorted
    files changed

--- NotePadApplication/app/src/main/java/com/example/myapplication/MainActivity.java
+++ NotePadApplication/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,19 +1,34 @@
 package com.example.myapplication;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 public class MainActivity extends AppCompatActivity {
+
     private EditText mloginemail,mloginpassword;
     private RelativeLayout mlogin,mgotosignup;
-    private TextView mgotoforgotpassword;
+    private  TextView mgotoforgotpassword;
+
+    private FirebaseAuth firebaseAuth;
+
+    ProgressBar mprogressbarofmainactivity;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -26,20 +41,31 @@ public class MainActivity extends AppCompatActivity {
         mlogin=findViewById(R.id.login);
         mgotoforgotpassword=findViewById(R.id.gotoforgotpassword);
         mgotosignup=findViewById(R.id.gotosignup);
+        //mprogressbarofmainactivity=findViewById(R.id.progressbarofmainactivity);
+
+        firebaseAuth=FirebaseAuth.getInstance();
+        FirebaseUser firebaseUser=firebaseAuth.getCurrentUser();
+
+//        if(firebaseUser!=null)
+//        {
+//            finish();
+//            startActivity(new Intent(MainActivity.this,notesactivity.class));
+//        }
 
         mgotosignup.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(MainActivity.this, signup.class));
+                startActivity(new Intent(MainActivity.this,signup.class));
             }
         });
 
-        mgotoforgotpassword.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(MainActivity.this, forgotpassword.class));
-            }
-        });
+//        mgotoforgotpassword.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                startActivity(new Intent(MainActivity.this,fogotpassword.class));
+//            }
+//        });
+
 
         mlogin.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -47,13 +73,60 @@ public class MainActivity extends AppCompatActivity {
                 String mail=mloginemail.getText().toString().trim();
                 String password=mloginpassword.getText().toString().trim();
 
-                if(mail.isEmpty()||password.isEmpty()){
-                    Toast.makeText(getApplicationContext(), "All Fields are Required", Toast.LENGTH_SHORT).show();
+                if(mail.isEmpty()|| password.isEmpty())
+                {
+                    Toast.makeText(getApplicationContext(),"All Field Are Required",Toast.LENGTH_SHORT).show();
+
                 }
-                else {
-                    //login the user
+                else
+                {
+                    // login the user
+                    mprogressbarofmainactivity.setVisibility(View.VISIBLE);
+
+                    firebaseAuth.signInWithEmailAndPassword(mail,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+
+
+                            if(task.isSuccessful())
+                            {
+                                checkmailverfication();
+                            }
+                            else
+                            {
+                                Toast.makeText(getApplicationContext(),"Account Doesn't Exist",Toast.LENGTH_SHORT).show();
+                                mprogressbarofmainactivity.setVisibility(View.INVISIBLE);
+                            }
+
+
+                        }
+                    });
                 }
             }
         });
+
     }
+
+
+    private void checkmailverfication()
+    {
+        FirebaseUser firebaseUser=firebaseAuth.getCurrentUser();
+
+        if(firebaseUser.isEmailVerified()==true)
+        {
+            Toast.makeText(getApplicationContext(),"Logged In",Toast.LENGTH_SHORT).show();
+            finish();
+            //startActivity(new Intent(MainActivity.this,notesactivity.class));
+        }
+        else
+        {
+            mprogressbarofmainactivity.setVisibility(View.INVISIBLE);
+            Toast.makeText(getApplicationContext(),"Verify your mail first",Toast.LENGTH_SHORT).show();
+            firebaseAuth.signOut();
+        }
+    }
+
+
+
+
 }
--- NotePadApplication/app/src/main/java/com/example/myapplication/signup.java
+++ NotePadApplication/app/src/main/java/com/example/myapplication/signup.java
@@ -5,8 +5,6 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.text.TextUtils;
-import android.util.Patterns;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.RelativeLayout;
@@ -19,15 +17,13 @@ import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
-import org.w3c.dom.Text;
-
-import java.util.regex.Pattern;
-
 public class signup extends AppCompatActivity {
+
 
     private EditText msignupemail,msignuppassword;
     private RelativeLayout msignup;
     private TextView mgotologin;
+
 
     private FirebaseAuth firebaseAuth;
 
@@ -37,7 +33,7 @@ public class signup extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
 
-//        getSupportActionBar().hide();
+        //getSupportActionBar().hide();
 
         msignupemail=findViewById(R.id.signupemail);
         msignuppassword=findViewById(R.id.signuppassword);
@@ -46,63 +42,85 @@ public class signup extends AppCompatActivity {
 
         firebaseAuth= FirebaseAuth.getInstance();
 
-        msignup.setOnClickListener(v -> {
-            String mail= msignupemail.toString().trim();
-            String password= msignuppassword.getText().toString().trim();
-            if(isValidEmail(mail)) {
-                if (isValidPassword(password)) {
-                    firebaseAuth.createUserWithEmailAndPassword(mail, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
-                        @Override
-                        public void onComplete(@NonNull Task<AuthResult> task) {
-                            if (task.isSuccessful()) {
-                                Toast.makeText(getApplicationContext(), "Registration Successful", Toast.LENGTH_SHORT).show();
-                                sendEmailVerification();
-                            } else {
-                                Toast.makeText(getApplicationContext(), "Failed to Register", Toast.LENGTH_SHORT).show();
-                            }
-                        }
-                    });
-                } else {
-                    Toast.makeText(this, "Invalid password", Toast.LENGTH_SHORT).show();
-                }
-            }
-            else{
-                Toast.makeText(this,"Invalid Email",Toast.LENGTH_SHORT).show();
+
+
+
+
+        mgotologin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(signup.this,MainActivity.class);
+                startActivity(intent);
             }
         });
-    }
 
-    private static boolean isValidPassword(CharSequence target) {
-        Pattern PASSWORD_PATTERN = Pattern.compile("[a-zA-Z0-9\\!\\@\\#\\$]{7,24}");
-        return (!TextUtils.isEmpty(target) && PASSWORD_PATTERN.matcher(target).matches());
-    }
+        msignup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
 
-    public static boolean isValidEmail(CharSequence target) {
-        return (!TextUtils.isEmpty(target) && Patterns.EMAIL_ADDRESS.matcher(target).matches());
-    }
+                String mail=msignupemail.getText().toString().trim();
+                String password=msignuppassword.getText().toString().trim();
 
+                if(mail.isEmpty() || password.isEmpty())
+                {
+                    Toast.makeText(getApplicationContext(),"All Fields are Required",Toast.LENGTH_SHORT).show();
+                }
+                else if(password.length()<7)
+                {
+                    Toast.makeText(getApplicationContext(),"Password Should Greater than 7 Digits",Toast.LENGTH_SHORT).show();
+                }
+                else
+                {
+                    /// registered the user to firebase
+
+                    firebaseAuth.createUserWithEmailAndPassword(mail,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+
+                            if(task.isSuccessful())
+                            {
+                                Toast.makeText(getApplicationContext(),"Registration Successful",Toast.LENGTH_SHORT).show();
+                                sendEmailVerification();
+                            }
+                            else
+                            {
+                                Toast.makeText(getApplicationContext(),"Failed To Register",Toast.LENGTH_SHORT).show();
+                            }
+
+
+                        }
+                    });
+
+                }
+
+            }
+        });
+
+
+    }
 
     //send email verification
     private void sendEmailVerification()
     {
-        FirebaseUser firebaseUser= firebaseAuth.getCurrentUser();
-
+        FirebaseUser firebaseUser=firebaseAuth.getCurrentUser();
         if(firebaseUser!=null)
         {
             firebaseUser.sendEmailVerification().addOnCompleteListener(new OnCompleteListener<Void>() {
                 @Override
                 public void onComplete(@NonNull Task<Void> task) {
-                    Toast.makeText(getApplicationContext(), "Verification Email is send , Verify and Log in Again", Toast.LENGTH_SHORT).show();
+                    Toast.makeText(getApplicationContext(),"Verification Email is Sent,Verify and Log In Again",Toast.LENGTH_SHORT).show();
                     firebaseAuth.signOut();
                     finish();
-                    startActivity(new Intent(signup.this, MainActivity.class));
+                    startActivity(new Intent(signup.this,MainActivity.class));
                 }
             });
-
         }
-    else
-    {
-        Toast.makeText(getApplicationContext(), "Failed to send Verification", Toast.LENGTH_SHORT).show();
+
+        else
+        {
+            Toast.makeText(getApplicationContext(),"Failed To Send Verification Email",Toast.LENGTH_SHORT).show();
+        }
     }
-    }
+
+
 }
