c11cdf30e17cc0a81c177549cc759567f008570a
gauri02saxena <gauri.saxena7379@gmail.com>
2023-05-16 19:09:06 +0530

    Created notedetails

---
+++ NotePadApplication/app/src/main/java/com/example/myapplication/notedetails.java
@@ -0,0 +1,57 @@
+package com.example.notesapptutorial;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.graphics.drawable.Drawable;
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+import androidx.appcompat.widget.Toolbar;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import java.util.Random;
+
+public class notedetails extends AppCompatActivity {
+
+
+    private TextView mtitleofnotedetail,mcontentofnotedetail;
+    FloatingActionButton mgotoeditnote;
+
+    ImageView imageView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_notedetails);
+        mtitleofnotedetail=findViewById(R.id.titleofnotedetail);
+        mcontentofnotedetail=findViewById(R.id.contentofnotedetail);
+        mgotoeditnote=findViewById(R.id.gotoeditnote);
+        Toolbar toolbar=findViewById(R.id.toolbarofnotedetail);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        imageView = findViewById(R.id.backGround);
+        imageView.setImageResource(getRandomImageBackground());
+        Intent data=getIntent();
+
+        mgotoeditnote.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(v.getContext(),editnoteactivity.class);
+                intent.putExtra("title",data.getStringExtra("title"));
+                intent.putExtra("content",data.getStringExtra("content"));
+                intent.putExtra("noteId",data.getStringExtra("noteId"));
+                v.getContext().startActivity(intent);
+            }
+        });
+
+        mcontentofnotedetail.setText(data.getStringExtra("content"));
+        mtitleofnotedetail.setText(data.getStringExtra("title"));
+    }
+
